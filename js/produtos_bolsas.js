8ae59e4ee4249a368903d9dd53db48e8447d103f
SamaraBilby <samarabilby@hotmail.com>
2024-05-27 12:18:53 -0300

    add botão para selecionar categorias e união de balsa e oculos em uma página só

--- js/produtos_bolsas.js
+++ js/produtos_bolsas.js
@@ -671,13 +671,351 @@ const produtos = [
         price: 29.00,
         img: './img/product/bolsas/bolsa-phillip-kollin-yz2120379-dark-yellow.jpg',
         description: ""
-    }, 
+    },
+    {
+    id: 56,
+    code: 79852,
+    title: "Mochila Constantim K9576W",
+    category: "Acessórios",
+    subcategory: "Bolsa",
+    model:"K9576W",
+    old_price:1188.00,
+    price: 29.00,
+    img: './img/product/bolsas/mochila-79852.jpg',
+    description: ""
+    },
+    {
+        id: 57,
+        code: 79851,
+        title: "Mochila Constantim K9590W-E",
+        category: "Acessórios",
+        subcategory: "Bolsa",
+        model:"K9590W-E",
+        old_price:1188.00,
+        price: 29.00,
+        img: './img/product/bolsas/mochila-79851.jpg',
+        description: ""
+        },
+    {
+        id: 1,
+        code:77239,
+        title: "Óculos de Sol Carmim CRM42289 142 C4",
+        color: "Fumê Degradê com Proteção contra raios UVA/UVB",
+        category: "Acessórios",
+        subcategory: "Óculos de Sol",
+        model:"C4",
+        old_price:313.00,
+        price: 29.00,
+        img: './img/product/oculos/oculos-de-sol-carmim-crm42289-142-c4.jpg',
+        description: ""
+    },
+
+    {
+        id: 2,
+        code:77244,
+        title: "Óculos de Sol Carmim CRM42483 C5",
+        color: "Fumê Degradê com Proteção contra raios UVA/UVB",
+        category: "Acessórios",
+        subcategory: "Óculos de Sol",
+        model:"C5",
+        old_price:1188.00,
+        price: 29.00,
+        img: './img/product/oculos/oculos-de-sol-carmim-crm42483-c5.jpg',
+        description: ""
+    },
+    {
+        id: 3,
+        code:79850,
+        title: "Óculos de Sol Constantim TS6081-C1",
+        color: "Degradê Fumê",
+        category: "Acessórios",
+        subcategory: "Óculos de Sol",
+        model:"C1",
+        old_price:1188.00,
+        price: 29.00,
+        img: './img/product/oculos/oculos-de-sol-constantim-ts6081c1.jpg',
+        description: ""
+    },
+    {
+        id: 5,
+        code:79926,
+        title: "Óculos de Sol Constantim TS6081-C2",
+        color: "Espelhadas",
+        category: "Acessórios",
+        subcategory: "Óculos de Sol",
+        model:"C2",
+        old_price:1188.00,
+        price: 29.00,
+        img: './img/product/oculos/oculos-de-sol-constantim-ts6081c2.jpg',
+        description: ""
+    },
+    {
+        id: 6,
+        code:79925,
+        title: "Óculos de Sol Constantim TS6081-C3",
+        color: "Degradê Marrom",
+        category: "Acessórios",
+        subcategory: "Óculos de Sol",
+        model:"C3",
+        old_price:1188.00,
+        price: 29.00,
+        img: './img/product/oculos/oculos-de-sol-constantim-ts6081c3.jpg',
+        description: ""
+    },
+    {
+        id: 7,
+        code:79932,
+        title: "Óculos de Sol Constantim TS7007-C1",
+        color: "Degradê Fumê",
+        category: "Acessórios",
+        subcategory: "Óculos de Sol",
+        model:"C1",
+        old_price:1188.00,
+        price: 29.00,
+        img: './img/product/oculos/oculos-de-sol-constantim-ts7007c1.jpg',
+        description: ""
+    },
+    {
+        id: 8,
+        code:79934,
+        title: "Óculos de Sol Constantim TS7007-C2",
+        color: "Espelhadas Amarelas",
+        category: "Acessórios",
+        subcategory: "Óculos de Sol",
+        model:"C2",
+        old_price:1188.00,
+        price: 29.00,
+        img: './img/product/oculos/oculos-de-sol-constantim-ts7007c2.jpg',
+        description: ""
+    },
+    {
+        id: 9,
+        code:79933,
+        title: "Óculos de Sol Constantim TS7007-C3",
+        color: "Espelhdas",
+        category: "Acessórios",
+        subcategory: "Óculos de Sol",
+        model:"C3",
+        old_price:1188.00,
+        price: 29.00,
+        img: './img/product/oculos/oculos-de-sol-constantim-ts7007c3.jpg',
+        description: ""
+    },
+    {
+        id: 10,
+        code:79938,
+        title: "Óculos de Sol Constantim TS7008-C1",
+        color: "Espelhadas",
+        category: "Acessórios",
+        subcategory: "Óculos de Sol",
+        model: "C1",
+        old_price:1188.00,
+        price: 29.00,
+        img: './img/product/oculos/oculos-de-sol-constantim-ts7008c1.jpg',
+        description: ""
+    },
+    {
+        id: 11,
+        code: 79935,
+        title: "Óculos de Sol Constantim TS7008-C2",
+        color: "Espelhadas Azuis",
+        category: "Acessórios",
+        subcategory: "Óculos de Sol",
+        model: "C2",
+        old_price:1188.00,
+        price: 29.00,
+        img: './img/product/oculos/oculos-de-sol-constantim-ts7008c2.jpg',
+        description: ""
+    },
+    {
+        id: 12,
+        code:79937,
+        title: "Óculos de Sol Constantim TS7008-C3",
+        color: "Marrom",
+        category: "Acessórios",
+        subcategory: "Óculos de Sol",
+        model:"C3",
+        old_price:1188.00,
+        price: 29.00,
+        img: './img/product/oculos/oculos-de-sol-constantim-ts7008c3.jpg',
+        description: ""
+    },
+    {
+        id: 13,
+        code:79936,
+        title: "Óculos de Sol Constantim TS7008-C4",
+        color:"Espelhadas Laranjas",
+        category: "Acessórios",
+        subcategory: "Óculos de Sol",
+        model:"C4",
+        old_price:1188.00,
+        price: 29.00,
+        img: './img/product/oculos/oculos-de-sol-constantim-ts7008c4.jpg',
+        description: ""
+    },
+    {
+        id: 14,
+        code:79929,
+        title: "Óculos de Sol Constantim TS7010-C1",
+        color:"Furta Cor Espelhadas",
+        category: "Acessórios",
+        subcategory: "Óculos de Sol",
+        model:"C1",
+        old_price:1188.00,
+        price: 29.00,
+        img: './img/product/oculos/oculos-de-sol-constantim-ts7010c1.jpg',
+        description: ""
+    },
+    {
+        id: 15,
+        code:79927,
+        title: "Óculos de Sol Constantim TS7010-C2",
+        color:"Azuis Espelhadas",
+        category: "Acessórios",
+        subcategory: "Óculos de Sol",
+        model:"C2",
+        old_price:1188.00,
+        price: 29.00,
+        img: './img/product/oculos/oculos-de-sol-constantim-ts7010c2.jpg',
+        description: ""
+    },
+    {
+        id: 16,
+        code:79930,
+        title: "Óculos de Sol Constantim TS7010-C3",
+        color:"Fumê Degradê",
+        category: "Acessórios",
+        subcategory: "Óculos de Sol",
+        model:"C3",
+        old_price:1188.00,
+        price: 29.00,
+        img: './img/product/oculos/oculos-de-sol-constantim-ts7010c3.jpg',
+        description: ""
+    },
+    {
+        id: 18,
+        code: 79931,
+        title: "Óculos de Sol Constantim TS7010-C4",
+        color:"Azul Espelhada",
+        category: "Acessórios",
+        subcategory: "Óculos de Sol",
+        model:"C4",
+        old_price:1188.00,
+        price: 29.00,
+        img: './img/product/oculos/oculos-de-sol-constantim-ts7010c4.jpg',
+        description: ""
+    },
+    {
+        id: 19,
+        code:85981,
+        title: "Óculos de Sol Phillip Kollin 2609 C3",
+        color:"UV400 Marrom Degradê",
+        category: "Acessórios",
+        subcategory: "Óculos de Sol",
+        model:"C3",
+        old_price:1188.00,
+        price: 29.00,
+        img: './img/product/oculos/oculos-de-sol-phillip-kollin-2609-c3.jpg',
+        description: ""
+    },
+    {
+        id: 20,
+        code: 86007,
+        title: "Óculos de Sol Phillip Kollin 8098 C2",
+        color:"UV400 Roxa",
+        category: "Acessórios",
+        subcategory: "Óculos de Sol",
+        model:"C2",
+        old_price:1188.00,
+        price: 29.00,
+        img: './img/product/oculos/oculos-de-sol-phillip-kollin-8098-c2.jpg',
+        description: ""
+    },
+    {
+        id: 21,
+        code:86003,
+        title: "Óculos de Sol Phillip Kollin 8098 C5",
+        color:"UV400 Marrom",
+        category: "Acessórios",
+        subcategory: "Óculos de Sol",
+        model:"C5",
+        old_price:1188.00,
+        price: 29.00,
+        img: './img/product/oculos/oculos-de-sol-phillip-kollin-8098-c5.jpg',
+        description: ""
+    },
+    {
+        id: 22,
+        code: 78851,
+        title: "Óculos de Sol Phillip Kollin C3285 C3",
+        color: "Degradê Fumê",
+        category: "Acessórios",
+        subcategory: "Óculos de Sol",
+        model:"C3",
+        old_price:1188.00,
+        price: 29.00,
+        img: './img/product/oculos/oculos-de-sol-phillip-kollin-c3285-c3.jpg',
+        description: ""
+    },
+    {
+        id: 23,
+        code: 78862,
+        title: "Óculos de Sol Phillip Kollin PZ8842 C5",
+        color: "Degrade Marrom",
+        category: "Acessórios",
+        subcategory: "Óculos de Sol",
+        model:"C5",
+        old_price:1188.00,
+        price: 29.00,
+        img: './img/product/oculos/oculos-de-sol-phillip-kollin-pz8842-c5.jpg',
+        description: ""
+    },
+    {
+        id: 24,
+        code:76715,
+        title: "Óculos de Sol Phillip Kollin RHSO-H202-C04",
+        color: "Marrom Degradê",
+        category: "Acessórios",
+        subcategory: "Óculos de Sol",
+        model:"RHSO-H202",
+        old_price:1188.00,
+        price: 29.00,
+        img: './img/product/oculos/oculos-de-sol-phillip-kollin-rhsoh202c04.jpg',
+        description: ""
+    },
+    {
+        id: 25,
+        code:76716,
+        title: "Óculos de Sol Phillip Kollin RHSO-H214-C03",
+        color: "Marrom",
+        category: "Acessórios",
+        subcategory: "Óculos de Sol",
+        model:"RHSO-H214",
+        old_price:1188.00,
+        price: 29.00,
+        img: './img/product/oculos/oculos-de-sol-phillip-kollin-rhsoh214c03.jpg',
+        description: ""
+    },
+    {
+        id: 26,
+        code:76959,
+        title: "Óculos de Sol Victor Hugo SH1742 0Z10",
+        color: "Marrom Degradê",
+        category: "Acessórios",
+        subcategory: "Óculos de Sol",
+        model:"SH1742",
+        old_price:1188.00,
+        price: 29.00,
+        img: './img/product/oculos/oculos-de-sol-victor-hugo-sh1742-0z10.jpg',
+        description: ""
+    } 
 
 ];
 
 const produtosContainer = document.querySelector('#produtosContainer');
 const quantidadesProdutos = document.querySelector(".quantidadesProdutos");
 const buscaInput = document.querySelector(".pesquisaInput");
+const filterButtons = document.querySelectorAll(".filter-btn");
 
 
 
@@ -781,3 +1119,24 @@ document.body.addEventListener('click', function(event){
 
 
 });
+
+filterButtons.forEach(button => {
+    button.addEventListener('click', (event) => {
+        filterButtons.forEach(btn => btn.classList.remove('selected'));
+        event.target.classList.add('selected');
+
+        const filter = event.target.getAttribute('data-filter');
+        let produtosFiltrados;
+
+        if (filter === 'all') {
+            produtosFiltrados = produtos;
+        } else {
+            produtosFiltrados = produtos.filter(produto => produto.subcategory === filter);
+        }
+
+        ListaEQuantidadesProdutos(produtosFiltrados);
+    });
+});
+
+// Renderiza todos os produtos inicialmente
+ListaEQuantidadesProdutos(produtos);
